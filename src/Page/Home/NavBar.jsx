44894c34db493f75869ed08ba18098e7971555c3
jonayotte <jayotte@uvic.ca>
2024-05-21 00:52:57 -0700

    update
    added background colors, portfolio items, removed links for now

--- src/Page/Home/NavBar.jsx
+++ src/Page/Home/NavBar.jsx
@@ -37,11 +37,11 @@ function Navbar() {
                     style={{ width: '75px', height: '75px' }}
                 />
             </div>
-            <a className={`nav__hamburger ${navActive ? "active" : ""}`} onClick={toggleNav} >
+            <button className={`nav__hamburger ${navActive ? "active" : ""}`} onClick={toggleNav} >
                 <span className="nav__hamburger__line"></span>
                 <span className="nav__hamburger__line"></span>
                 <span className="nav__hamburger__line"></span>
-            </a>
+            </button>
 
             <div className={`navbar--items ${navActive ? "active" : ""}`}>
                 <ul>
